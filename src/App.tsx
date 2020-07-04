a35dcbccd8fab6b50d7ca4a08d5ce1ebf26b29ea
Роман Журавлёв <snelsithepirate@gmail.com>
2020-07-04 11:47:54 +0300

    Remove unused div

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import styled from "styled-components";
 
 import { Board, GitHubLink } from "components";
 
-const Wrapper = styled.div``;
-
 export const App = () => (
   <main>
     <GitHubLink />
-    <Wrapper>
-      <Board />
-    </Wrapper>
+
+    <Board />
   </main>
 );
